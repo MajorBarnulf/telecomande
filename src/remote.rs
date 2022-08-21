2662c01b51b4a6f63b24f231aca6649f9e99462d
JOLIMAITRE Matthieu <matthieu@imagevo.fr>
2022-08-21 20:18:41 +0200

    relaxed requirements on remote debug by implementing manually

--- src/remote.rs
+++ src/remote.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use tokio::sync::mpsc::{self, error::SendError};
 
 use crate::traits::Processor;
@@ -5,7 +7,7 @@ use crate::traits::Processor;
 /// Represents a shareable/clonable remote to a task running a [`crate::Processor`].
 /// Used to send [`crate::Command`] to that task.
 /// Initially constructed by the [`crate::Handle`] returned on the [`crate::Executor::spawn`] of the task.
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct Remote<P>
 where
     P: Processor,
@@ -24,5 +26,19 @@ where
     /// Sends a [`crate::Command`] to the running [`crate::Processor`] for it to handle.
     pub fn send(&self, command: P::Command) -> Result<(), SendError<P::Command>> {
         self.sender.send(command)
+    }
+
+    /// Returns a [`crate::Remote`] able to send [`crate::Command`] to the [`crate::Processor`] of the running task.
+    pub fn remote(&self) -> Self {
+        Self::new(self.sender.clone())
+    }
+}
+
+impl<P> Debug for Remote<P>
+where
+    P: Processor,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Remote").field("sender", &"...").finish()
     }
 }
